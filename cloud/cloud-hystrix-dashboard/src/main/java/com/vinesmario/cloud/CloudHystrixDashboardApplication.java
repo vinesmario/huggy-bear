eb591cb8f806cfb9b6cbf0d5daeacfdabb147e06
vinesmario <645211440@qq.com>
2019-04-09 18:13:25 +0800

    feature: add hystrix dashboard

---
+++ cloud/cloud-hystrix-dashboard/src/main/java/com/vinesmario/cloud/CloudHystrixDashboardApplication.java
@@ -0,0 +1,23 @@
+package com.vinesmario.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @RefreshScope 当application.yml配置文件发生变化的时候，
+ * 不需要手动的进行重启，调用post请求http://host:port/refresh,就会加载新的配置文件，
+ * 正在访问的客户并不影响还是使用旧的配置文件，后来的用户会使用新的配置文件。
+ */
+
+@SpringCloudApplication
+@EnableHystrixDashboard
+//@RefreshScope
+public class CloudHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudHystrixDashboardApplication.class, args);
+    }
+
+
+}
--- microservice/microservice-uaa-parent/microservice-uaa-server/src/main/java/com/vinesmario/microservice/server/uaa/UaaServerApplication.java
+++ microservice/microservice-uaa-parent/microservice-uaa-server/src/main/java/com/vinesmario/microservice/server/uaa/UaaServerApplication.java
@@ -3,14 +3,13 @@ package com.vinesmario.microservice.server.uaa;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
-@EnableFeignClients
 @ComponentScan("com.vinesmario.microservice")
 @MapperScan("com.vinesmario.microservice.server")
+@EnableFeignClients("com.vinesmario.microservice")
 public class UaaServerApplication {
 
     public static void main(String[] args) {
