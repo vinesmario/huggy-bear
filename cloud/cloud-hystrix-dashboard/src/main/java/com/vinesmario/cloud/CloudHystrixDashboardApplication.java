90f62e32be7aef59a79f162da5d2e8e622159d72
vinesmario <645211440@qq.com>
2019-04-15 18:59:52 +0800

    feature: add admin server

--- cloud/cloud-hystrix-dashboard/src/main/java/com/vinesmario/cloud/CloudHystrixDashboardApplication.java
+++ cloud/cloud-hystrix-dashboard/src/main/java/com/vinesmario/cloud/CloudHystrixDashboardApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  * @RefreshScope 当application.yml配置文件发生变化的时候，
  * 不需要手动的进行重启，调用post请求http://host:port/refresh,就会加载新的配置文件，
  * 正在访问的客户并不影响还是使用旧的配置文件，后来的用户会使用新的配置文件。
+ *
+ * 主页访问地址 http://hostname:port/hystrix
  */
 
 @SpringBootApplication
