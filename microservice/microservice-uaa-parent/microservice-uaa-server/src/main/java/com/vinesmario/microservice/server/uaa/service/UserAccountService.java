1a312b1d317473d35a9fadc2f433fdf504be5879
vinesmario <645211440@qq.com>
2019-07-10 15:45:26 +0800

    refactor: crud service

--- microservice/microservice-uaa-parent/microservice-uaa-server/src/main/java/com/vinesmario/microservice/server/uaa/service/UserAccountService.java
+++ microservice/microservice-uaa-parent/microservice-uaa-server/src/main/java/com/vinesmario/microservice/server/uaa/service/UserAccountService.java
@@ -5,7 +5,7 @@ import com.vinesmario.microservice.client.uaa.dto.UserAccountDto;
 import com.vinesmario.microservice.client.uaa.dto.condition.UserAccountConditionDto;
 import com.vinesmario.microservice.server.common.constant.DictConstant;
 import com.vinesmario.microservice.server.common.persistence.mybatis.BaseExample;
-import com.vinesmario.microservice.server.common.service.mybatis.BaseService;
+import com.vinesmario.microservice.server.common.service.mybatis.impl.BaseService;
 import com.vinesmario.microservice.server.uaa.entity.UserAccount;
 import com.vinesmario.microservice.server.uaa.mapper.UserAccountMapper;
 import com.vinesmario.microservice.server.uaa.mapstruct.UserAccountMapStruct;
