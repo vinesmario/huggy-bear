3dc648c6379217ff12c208b9328d2b80fa5007cd
vinesmario <645211440@qq.com>
2019-07-16 14:25:43 +0800

    fix: common

--- common/common-server-web/src/main/java/com/vinesmario/microservice/server/common/service/mybatis/CrudService.java
+++ common/common-server-web/src/main/java/com/vinesmario/microservice/server/common/service/mybatis/CrudService.java
@@ -1,12 +1,17 @@
 package com.vinesmario.microservice.server.common.service.mybatis;
 
+import com.vinesmario.microservice.client.common.dto.condition.ConditionDto;
+
 public interface CrudService<T, PK> extends ReadOnlyService<T, PK> {
 
     void create(T t);
 
     void delete(T t);
 
-    void deleteIterable(Iterable<T> iterable);
+    void delete(ConditionDto conditionDto);
 
     void modify(T t);
+
+    void modify(T t, ConditionDto conditionDto);
+
 }
