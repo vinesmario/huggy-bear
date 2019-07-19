3a958ab0a8b00ae23498270266cf0b1c8ab388a0
vinesmario <645211440@qq.com>
2019-07-19 23:26:46 +0800

    fix: oauth2 feign client interceptor configuration

--- microservice/microservice-uaa-parent/microservice-uaa-server/src/main/java/com/vinesmario/microservice/server/UaaServerApplication.java
+++ microservice/microservice-uaa-parent/microservice-uaa-server/src/main/java/com/vinesmario/microservice/server/UaaServerApplication.java
@@ -1,6 +1,6 @@
 package com.vinesmario.microservice.server;
 
-import com.vinesmario.microservice.client.common.web.feign.interceptor.OAuth2UserFeignClientConfiguration;
+import com.vinesmario.microservice.client.common.web.feign.interceptor.OAuth2FeignClientConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
@@ -8,11 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringCloudApplication
-//@ComponentScan("com.vinesmario.microservice")
 @ComponentScan(
         basePackages = {"com.vinesmario.microservice"},
         excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-                classes = {OAuth2UserFeignClientConfiguration.class})
+                classes = {OAuth2FeignClientConfiguration.class})
 )
 @MapperScan("com.vinesmario.microservice.server")
 public class UaaServerApplication {
