8c270008a50b150cc7a2d34066dbf6128596845d
vinesmario <645211440@qq.com>
2019-09-10 18:12:23 +0800

    feature: upgrade spring cloud version

--- common/common-microservice-client/src/main/java/com/vinesmario/microservice/client/common/web/feign/interceptor/QueryConditionInterceptor.java
+++ common/common-microservice-client/src/main/java/com/vinesmario/microservice/client/common/web/feign/interceptor/QueryConditionInterceptor.java
@@ -2,6 +2,7 @@ package com.vinesmario.microservice.client.common.web.feign.interceptor;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Request;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
@@ -16,56 +17,56 @@ import java.util.*;
 @Configuration
 public class QueryConditionInterceptor implements RequestInterceptor {
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Override
-	public void apply(RequestTemplate template) {
-		// 因为feign 不支持 GET 方法传 POJO,
-		// 将body转key=value
-		if (template.method().equalsIgnoreCase("GET") && template.body() != null) {
-			try {
-				JsonNode jsonNode = objectMapper.readTree(template.body());
-				template.body(null);
+    @Override
+    public void apply(RequestTemplate template) {
+        // 因为feign 不支持 GET 方法传 POJO,
+        // 将body转key=value
+        if (template.method().equalsIgnoreCase("GET") && template.body() != null) {
+            try {
+                JsonNode jsonNode = objectMapper.readTree(template.requestBody().asBytes());
+                template.body((Request.Body) null);
 
-				Map<String, Collection<String>> queries = new HashMap<>();
-				buildCondition(jsonNode, "", queries);
-				template.queries(queries);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                Map<String, Collection<String>> queries = new HashMap<>();
+                buildCondition(jsonNode, "", queries);
+                template.queries(queries);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private void buildCondition(JsonNode jsonNode, String path, Map<String, Collection<String>> queries) {
-		if (!jsonNode.isContainerNode()) {   // 叶子节点
-			if (jsonNode.isNull()) {
-				return;
-			}
-			Collection<String> values = queries.get(path);
-			if (null == values) {
-				values = new ArrayList<>();
-				queries.put(path, values);
-			}
-			values.add(jsonNode.asText());
+    private void buildCondition(JsonNode jsonNode, String path, Map<String, Collection<String>> queries) {
+        if (!jsonNode.isContainerNode()) {   // 叶子节点
+            if (jsonNode.isNull()) {
+                return;
+            }
+            Collection<String> values = queries.get(path);
+            if (null == values) {
+                values = new ArrayList<>();
+                queries.put(path, values);
+            }
+            values.add(jsonNode.asText());
 //			log.info("set param {}={}", path, jsonNode.asText());
-			return;
-		}
-		if (jsonNode.isArray()) {   // 数组节点
-			Iterator<JsonNode> it = jsonNode.elements();
-			while (it.hasNext()) {
-				buildCondition(it.next(), path, queries);
-			}
-		} else {
-			Iterator<Map.Entry<String, JsonNode>> it = jsonNode.fields();
-			while (it.hasNext()) {
-				Map.Entry<String, JsonNode> entry = it.next();
-				if (StringUtils.hasText(path)) {
-					buildCondition(entry.getValue(), path + "." + entry.getKey(), queries);
-				} else {  // 根节点
-					buildCondition(entry.getValue(), entry.getKey(), queries);
-				}
-			}
-		}
-	}
+            return;
+        }
+        if (jsonNode.isArray()) {   // 数组节点
+            Iterator<JsonNode> it = jsonNode.elements();
+            while (it.hasNext()) {
+                buildCondition(it.next(), path, queries);
+            }
+        } else {
+            Iterator<Map.Entry<String, JsonNode>> it = jsonNode.fields();
+            while (it.hasNext()) {
+                Map.Entry<String, JsonNode> entry = it.next();
+                if (StringUtils.hasText(path)) {
+                    buildCondition(entry.getValue(), path + "." + entry.getKey(), queries);
+                } else {  // 根节点
+                    buildCondition(entry.getValue(), entry.getKey(), queries);
+                }
+            }
+        }
+    }
 }
