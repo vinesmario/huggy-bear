6a38dee2cbc19ea47ae46049c3285ff9e363f9c9
vinesmario <645211440@qq.com>
2019-10-16 00:25:51 +0800

    fix: oauth2 additional information

---
+++ industry/industry-generic/generic-business-infrastructure-parent/generic-business-infrastructure-client/src/main/java/com/vinesmario/microservice/client/movable/dto/IPCameraDTO.java
@@ -0,0 +1,7 @@
+package com.vinesmario.microservice.client.movable.dto;
+
+/**
+ * 网络摄像机
+ */
+public class IPCameraDTO {
+}
---
+++ industry/industry-generic/generic-business-infrastructure-parent/generic-business-infrastructure-client/src/main/java/com/vinesmario/microservice/client/movable/dto/NetworkVideoRecorderDTO.java
@@ -0,0 +1,7 @@
+package com.vinesmario.microservice.client.movable.dto;
+
+/**
+ * 网络录像机
+ */
+public class NetworkVideoRecorderDTO {
+}
